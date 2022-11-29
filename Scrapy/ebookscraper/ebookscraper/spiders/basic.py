2435356234887ebb00b1f11e00259f28222f1f3a
fynis96 <c.hearth96@gmail.com>
2022-11-29 08:38:41 -0500

    Started proceeding Scrapy tutorial, initialized project and spider

--- Scrapy/ebookscraper/ebookscraper/spiders/basic.py
+++ Scrapy/ebookscraper/ebookscraper/spiders/basic.py
@@ -6,4 +6,13 @@ class EbookSpider(scrapy.Spider):
     start_urls = ['http://books.toscrape.com/']
 
     def parse(self, response):
-        pass
+        ebooks = response.css('article.product_pod')
+        
+        for ebook in ebooks:
+            title = ebook.css('h3 a').attrib['title']
+            price = ebook.css('p.price_color::text').get()
+            
+            yield {
+                "title": title, "price": price
+            }
+            
