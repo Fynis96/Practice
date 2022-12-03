325652b37f8bfed28b3391a4112e4d9d02a3903f
fynis96 <c.hearth96@gmail.com>
2022-12-03 09:07:33 -0500

    Added page nav for sample site

--- Scrapy/ebookscraper/ebookscraper/spiders/ebook.py
+++ Scrapy/ebookscraper/ebookscraper/spiders/ebook.py
@@ -4,9 +4,23 @@ from scrapy.loader import ItemLoader
 
 class EbookSpider(scrapy.Spider):
     name = 'ebook'
-    start_urls = ['https://books.toscrape.com/']
+    start_urls = ['https://books.toscrape.com']
     cols = ['Title', 'Price']
-
+    
+    def __init__(self):
+        super().__init__()
+        self.page_count = 1
+        self.total_pages = 4
+        
+    def start_request(self):
+        base_url = "https://books.toscrape.com"
+        
+        while self.page_count <= self.total_pages:
+            yield scrapy.Request(
+                f"{base_url}/page-{self.page_count}.html"
+            )
+            self.page_count += 1
+        
     def parse(self, response):
         ebooks = response.css('article.product_pod')
         
@@ -17,4 +31,3 @@ class EbookSpider(scrapy.Spider):
             loader.add_css('price', 'p.price_color::text')
             
             yield loader.load_item()
-            
