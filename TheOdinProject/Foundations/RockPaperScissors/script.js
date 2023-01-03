ec4de30496bfc21dc5e1dd704e96a13d65f857bd
Fynis96 <C.hearth96@gmail.com>
2023-01-02 21:30:53 -0500

    Minor changes

--- TheOdinProject/Foundations/RockPaperScissors/script.js
+++ TheOdinProject/Foundations/RockPaperScissors/script.js
@@ -1,5 +1,8 @@
 let pcScore = 0;
 let cpScore = 0;
+let round = 0;
+let gameOver = false;
+
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random()*3) + 1;
@@ -41,16 +44,15 @@ function play(pc, cp) {
 
 document.querySelector(".buttonbox").addEventListener('click', function(event){
     //console.log(event);
-    if(event.target.innerHTML === 'R'){
-        computerPlay();
+    if(event.target.innerHTML === 'Rock'){
         play('Rock', computerPlay());
         console.log('rock');
     }
-    else if(event.target.innerHTML === 'P'){
+    else if(event.target.innerHTML === 'Paper'){
         play('Paper', computerPlay());
         console.log('paper');
     }
-    else if(event.target.innerHTML === 'S'){
+    else if(event.target.innerHTML === 'Scissors'){
         play('Scissors', computerPlay());
         console.log('scissors');
     }
@@ -60,4 +62,6 @@ const divScore = document.querySelector('.score');
 const divResult = document.querySelector('.results');
 
 
-
+//Function checkScore(p1, p2)
+//For styles, the pc/cp are huddled around big blackjack looking table which holds scoreboard, and the hands when they move forward. Uss CSS transition to push those forward nicely from both sides
+//add a win condition with the score, like a 2 out of 3 kind of situation so that it doesn't just go on infinitely
